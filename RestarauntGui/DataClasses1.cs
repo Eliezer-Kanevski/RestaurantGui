6aab57bb0df275fb608b26c2d547c7361719ce80
Eliezer-Kanevski <98493218+Eliezer-Kanevski@users.noreply.github.com>
2022-05-18 15:37:38 -0400

    add

---
+++ RestarauntGui/DataClasses1.cs
@@ -0,0 +1,18 @@
+namespace RestarauntGui
+{
+    partial class CUSTOMER_HAS_ORDER
+    {
+    }
+
+    partial class PERSON
+    {
+    }
+
+    partial class DataClasses1DataContext
+    {
+    }
+
+    partial class ORDER_HAS_ITEM
+    {
+    }
+}
